a9b85802d0608cfd9b286348d9eacc6acace4305
Mathias <mathias260405@gmail.com>
2026-07-27 23:41:58 -0300

    feat(server + docker): start postgres + .net ef in docker environment

---
+++ server/DevNotes.Api/Infrastructure/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using DevNotes.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace DevNotes.Api.Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration _configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("WebApiDatabase"));
+        }
+        public DbSet<Note> Notes {  get; set; }
+    }
+}
--- server/DevNotes.Api/Models/Note.cs
+++ server/DevNotes.Api/Models/Note.cs
@@ -2,7 +2,7 @@
 {
     public class Note
     {
-        public Guid Id = Guid.NewGuid();
+        public Guid Id { get; set; }
         public string? Title { get; set; }
         public string? Content { get; set; }
 
